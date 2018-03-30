3779966ee9f3f3921f9b7150af611c355c235827
Adnan Ansari <36989931+adnan07870@users.noreply.github.com>
2018-03-30 20:17:25 +0530

    Adding a Score window compliting the project as a app.

--- React_files/src/Card.js
+++ React_files/src/Card.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-const Card= ({waste,measure,Result})=> {
+const Card= ({onWaste, onWeight})=> {
 	return(
 		<div className='tc bg-light-green dib br3 pa3 ma2 bw2 shadow-5'>
 		   <h1 className='f2 i green'>Dumb Your Trash Here</h1>
-		   <input className='pa3 ba b--green bg-lightest-blue w-50' type='search' placeholder='Name of the Waste' />
-		   <input className='pa3 ba b--green bg-lightest-blue w-50' type='search' placeholder='Weight in kg' />
-           <input className='pa3 ma3 ba b--green bg-lightest-blue grow' type="submit" value="Submit" onClick={ResultBox} />
+		   <input className='pa3 ba b--green bg-lightest-blue w-50' type='text' placeholder='Name of the Waste' onChange={onWaste} />
+		   <input className='pa3 ba b--green bg-lightest-blue w-50' type='number' placeholder='Weight in kg' onChange={onWeight} />
 		</div>
 		);
 }
--- React_files/src/resultBox.js
+++ React_files/src/resultBox.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Score from './Score.js'
 
-const ResultBox= ({waste,measure}) => {
+const ResultBox= ({waste, weight}) => {
+
 	return(
-		<div>
-           <h1>{waste}</h1>
-           <h1>{measure}</h1>
+		<div className='result tc bg-light-green dib br3 pa3 ma2 bw2 shadow-5 '>
+		   <h1 className='f2 i green'>RESULT</h1>
+           <div><Score waste={waste} weight={weight} /></div>
 		</div>
 		);
 }
