22b2cc836839b8dac1c2ea2386aaa6b80d266476
GitGudJavaScript <39003861+GitGudJavaScript@users.noreply.github.com>
2019-05-19 16:23:29 +0100

    Improve header styling, make layout responsive, import tachyons so tachyon classes work

--- React_files/src/components/Cards/Card.js
+++ React_files/src/components/Cards/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Card= ({onWaste, onWeight})=> {
 	return(
-		<div className='tc bg-light-green dib br3 pa3 ma2 bw2 shadow-5'>
-		   <h1 className='f2 i green'>Dumb Your Trash Here</h1>
+		<div className='tc bg-light-green  pa3 ma2 bw2 shadow-5 trash-dump-card'>
+		   <h1 className='f2 black dump-trash'>Dump Your Trash Here</h1>
 		   <input className='pa3 ba b--green bg-lightest-blue w-50' type='text' placeholder='Name of the Waste' onChange={onWaste} />
 		   <input className='pa3 ba b--green bg-lightest-blue w-50' type='number' placeholder='Weight in kg' onChange={onWeight} />
 		</div>
--- React_files/src/index.js
+++ React_files/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
 
 ReactDOM.render(
   <App/>, document.getElementById('root'));
