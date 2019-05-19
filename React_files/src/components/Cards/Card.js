dc40e3cbf1b1e9c7b5ba31e7d0074a573b7e47ae
GitGudJavaScript <39003861+GitGudJavaScript@users.noreply.github.com>
2019-05-19 16:50:37 +0100

    Finish styling improvements and correct typos, delete misleading error message

--- React_files/src/components/Cards/Card.js
+++ React_files/src/components/Cards/Card.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Card= ({onWaste, onWeight})=> {
 	return(
-		<div className='tc bg-light-green  pa3 ma2 bw2 shadow-5 trash-dump-card'>
-		   <h1 className='f2 black dump-trash'>Dump Your Trash Here</h1>
-		   <input className='pa3 ba b--green bg-lightest-blue w-50' type='text' placeholder='Name of the Waste' onChange={onWaste} />
-		   <input className='pa3 ba b--green bg-lightest-blue w-50' type='number' placeholder='Weight in kg' onChange={onWeight} />
+		<div className='tc pa3 ma2 bw2 shadow-5 trash-dump-card'>
+		   <h1 className='f2 black dump-trash'>What are you recycling?</h1>
+		   <input className='pa3 w-30 waste-name' type='text' placeholder='Name of the Waste' onChange={onWaste} />
+		   <input className='pa3 w-30 weight-in-kg' type='number' placeholder='Weight in kg' onChange={onWeight} />
 		</div>
 		);
 }
--- React_files/src/components/ResultBox/ResultBox.js
+++ React_files/src/components/ResultBox/ResultBox.js
@@ -4,7 +4,7 @@ import Score from '../Score/Score'
 const ResultBox= ({waste, weight}) => {
 
 	return(
-		<div className='result tc bg-light-green pa3 ma2 bw2 shadow-5 results-card'>
+		<div className='result tc pa3 ma2 bw2 shadow-5 results-card'>
 		   <h1 className='f2 black'>RESULT</h1>
            <div><Score waste={waste} weight={weight} /></div>
 		</div>
