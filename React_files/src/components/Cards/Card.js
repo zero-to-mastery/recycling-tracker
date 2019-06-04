7aab934a8a5641b204587cfe2ce8445b84661cd3
Mario Bermudez Jr <mario.bermudez.jr@outlook.com>
2019-06-04 16:35:38 -0700

    [refactor] Add numeric description to input hint

--- React_files/src/components/Cards/Card.js
+++ React_files/src/components/Cards/Card.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
-const Card= ({onWaste, onWeight})=> {
-	return(
-		<div className='tc pa3 ma2 bw2 shadow-5 trash-dump-card'>
-		   <h1 className='f2 black dump-trash'>What are you recycling?</h1>
-		   <input className='pa3 w-30 waste-name' type='text' placeholder='Name of the Waste' onChange={onWaste} />
-		   <input className='pa3 w-30 weight-in-kg' type='number' placeholder='Weight in kg' onChange={onWeight} />
-		</div>
-		);
-}
+const Card = ({ onWaste, onWeight }) => {
+  return (
+    <div className="tc pa3 ma2 bw2 shadow-5 trash-dump-card">
+      <h1 className="f2 black dump-trash">What are you recycling?</h1>
+      <input
+        className="pa3 w-30 waste-name"
+        type="text"
+        placeholder="Name of the Waste"
+        onChange={onWaste}
+      />
+      <input
+        className="pa3 w-30 weight-in-kg"
+        type="number"
+        placeholder="Weight number in kg"
+        onChange={onWeight}
+      />
+    </div>
+  );
+};
 
 export default Card;
