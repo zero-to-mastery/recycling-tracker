264c254a51c9404a8e28cedaf090ff350ec86390
Mario Bermudez Jr <mario.bermudez.jr@outlook.com>
2019-06-04 15:45:34 -0700

    [fix] Fix bug where backspace on input would result in blank screen. Also fix class -> className on Score.js file.

--- React_files/src/components/Score/Score.js
+++ React_files/src/components/Score/Score.js
@@ -1,36 +1,32 @@
 import React from 'react';
 
-const Score = ({waste, weight}) => {
-          if(weight !== null && waste !== null) {
-               if((waste.length)&&weight>=100){
-                    return (
-                         <div>
-                           <h1>Congrats</h1>
-                           <h1>You are a Recycling King</h1>
-                         </div>
-                         );
-               }
-               else if(((waste.length)&&weight>=50) && (waste.length)&&weight<100){
-                    return(
-                         <div>
-                           <h1>Congrats</h1>
-                           <h1>You are a good Recycler</h1>
-                         </div>
-                         );
-               }
-               else if(((waste.length)&&weight<50) && (waste.length)&&weight>0){
-                    return(
-                         <div>
-                           <h1>Congrats</h1>
-                           <h1>You did good but do better next time.</h1>
-                         </div>
-                         );
-               }
-          } else {
-               return (
-                      <h1 class="initial-heading">Fill out the form above</h1>
-                    );
-          }
-	}
+const Score = ({ waste, weight }) => {
+  if (weight !== null && waste !== null) {
+    if (waste.length && weight >= 100) {
+      return (
+        <div>
+          <h1>Congrats</h1>
+          <h1>You are a Recycling King</h1>
+        </div>
+      );
+    } else if (waste.length && weight >= 50 && waste.length && weight < 100) {
+      return (
+        <div>
+          <h1>Congrats</h1>
+          <h1>You are a good Recycler</h1>
+        </div>
+      );
+    } else if (waste.length && weight < 50 && waste.length && weight > 0) {
+      return (
+        <div>
+          <h1>Congrats</h1>
+          <h1>You did good but do better next time.</h1>
+        </div>
+      );
+    }
+  } else {
+    return <h1 className="initial-heading">Fill out the form above</h1>;
+  }
+};
 
 export default Score;
--- React_files/src/containers/App.js
+++ React_files/src/containers/App.js
@@ -1,30 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 import Card from '../components/Cards/Card';
-import ResultBox from '../components/ResultBox/ResultBox'
-import Trash from '../components/Trash/Trash'
+import ResultBox from '../components/ResultBox/ResultBox';
+import Trash from '../components/Trash/Trash';
 
 class App extends Component {
-    state={
-            waste: null,
-            weight: null,
-            location:''
-          };
+  state = {
+    waste: null,
+    weight: null,
+    location: ''
+  };
 
-  onWasteChange= (event) => {
-    this.setState({waste: event.target.value});
-  }
+  onWasteChange = event => {
+    if (event.target.value === '') {
+      this.setState({ waste: null });
+    } else {
+      this.setState({ waste: event.target.value });
+    }
+  };
 
-  onWeightChange= (event) => {
-    this.setState({weight: event.target.value});
-  }
+  onWeightChange = event => {
+    if (event.target.value === '') {
+      this.setState({ weight: null });
+    } else {
+      this.setState({ weight: event.target.value });
+    }
+  };
 
   render() {
     return (
-      <div className='App'>
-         <Trash />
-         <Card onWaste={this.onWasteChange} onWeight={this.onWeightChange} />
-         <ResultBox waste={this.state.waste} weight={this.state.weight} />
+      <div className="App">
+        <Trash />
+        <Card onWaste={this.onWasteChange} onWeight={this.onWeightChange} />
+        <ResultBox waste={this.state.waste} weight={this.state.weight} />
       </div>
     );
   }
